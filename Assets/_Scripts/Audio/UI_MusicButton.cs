d560b9abb4d0b3521d575c33303f97bad635c9fa
Nikita <usualnik@gmail.com>
2025-08-06 14:51:50 +0300

    music|sound buttons

---
+++ Assets/_Scripts/Audio/UI_MusicButton.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_MusicButton : MonoBehaviour
+{
+    private Button _musicButton;
+    [SerializeField] private Image _buttonImage;
+
+    [SerializeField] private Sprite _musicOnSprite;
+    [SerializeField] private Sprite _musicOffSprite;
+
+    private bool _isPlaying;
+
+
+    private void Awake()
+    {
+        _musicButton = GetComponent<Button>();        
+        _isPlaying = true;
+       
+    }
+
+    private void Start()
+    {
+        _musicButton.onClick.AddListener(ToggleSound);
+
+    }
+
+    private void OnDestroy()
+    {
+        _musicButton.onClick.RemoveListener(ToggleSound);
+
+    }
+
+    private void ToggleSound()
+    {
+        BackgroundMusic.Instance.ToggleMute();
+
+        if (_isPlaying)
+        {
+            _buttonImage.sprite = _musicOffSprite;
+            _isPlaying = false;
+        }
+        else
+        {
+            _buttonImage.sprite = _musicOnSprite;
+            _isPlaying = true;
+        }
+    }
+}
---
+++ Assets/_Scripts/Audio/UI_SoundButton.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_SoundButton : MonoBehaviour
+{
+    private Button _soundButton;    
+    [SerializeField] private Image _buttonImage;
+
+    [SerializeField] private Sprite _soundOnSprite;
+    [SerializeField] private Sprite _soundOffSprite;
+
+    private bool _isPlaying;
+
+
+    private void Awake()
+    {
+        _soundButton = GetComponent<Button>();       
+
+        _isPlaying = true;
+
+    }
+
+    private void Start()
+    {
+        _soundButton.onClick.AddListener(ToggleSound);
+
+    }
+
+    private void OnDestroy()
+    {
+        _soundButton.onClick.RemoveListener(ToggleSound);
+
+    }
+
+    private void ToggleSound()
+    {
+
+        AudioManager.Instance.ToggleMute();
+
+
+        if (_isPlaying)
+        {
+            _buttonImage.sprite = _soundOffSprite;
+            _isPlaying = false;                       
+        }
+        else
+        {
+            
+            _buttonImage.sprite = _soundOnSprite;
+            _isPlaying = true;          
+
+        }
+    }
+}
